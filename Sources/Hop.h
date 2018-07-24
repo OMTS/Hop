e009c11368589069b7cd7fd55f248e006f11ba2d
poisson florent <poissonflorent@MacBook-Pro-de-poisson-2.local>
2018-07-24 22:27:31 +0200

    Initial Commit

---
+++ Sources/Hop.h
@@ -0,0 +1,19 @@
+//
+//  Hop.h
+//  Hop
+//
+//  Created by poisson florent on 24/07/2018.
+//  Copyright © 2018 poisson florent. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Hop.
+FOUNDATION_EXPORT double Hop_VersionNumber;
+
+//! Project version string for Hop.
+FOUNDATION_EXPORT const unsigned char Hop_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hop/PublicHeader.h>
+
+
