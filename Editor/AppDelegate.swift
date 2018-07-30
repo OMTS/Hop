f84382a111f235c04e0d472ba52d8e2fce25fe58
poisson florent <poissonflorent@MacBook-Pro-de-poisson-2.local>
2018-07-30 22:52:10 +0200

    Add mini editor for hop implementation test purpose

---
+++ Editor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Editor
+//
+//  Created by poisson florent on 30/07/2018.
+//  Copyright © 2018 poisson florent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Editor/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  Editor
+//
+//  Created by poisson florent on 30/07/2018.
+//  Copyright © 2018 poisson florent. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var sourceTextView: NSTextView!
+    @IBOutlet weak var logTextView: NSTextView!
+    @IBOutlet weak var runButton: NSButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        customize()
+        
+        if let initialScript = UserDefaults.standard.string(forKey: "edited-script") {
+            sourceTextView.string = initialScript
+        }
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    private func customize() {
+        sourceTextView.textColor = NSColor(calibratedWhite: 1, alpha: 1)
+        sourceTextView.font = NSFont(name: "Arial", size: 16)
+        sourceTextView.isGrammarCheckingEnabled = false
+        sourceTextView.isAutomaticTextCompletionEnabled = false
+        sourceTextView.isAutomaticSpellingCorrectionEnabled = false
+
+        logTextView.textColor = NSColor(calibratedWhite: 0.8, alpha: 1)
+        logTextView.font = NSFont(name: "Arial", size: 16)
+    }
+    
+    // MARK: - State managemenr
+    
+    private func runScript(_ script: String) {
+        // Auto save the current script
+        saveScript(script)
+        
+        runButton.isEnabled = false
+        logTextView.string = ""
+        
+        // Setup stdout callback
+        let messenger = Messenger()
+        messenger.subscribe(to: .stdout) { [weak self] (message) in
+            if let message = message.data as? String {
+                self?.displayLog(message: message)
+            }
+        }
+        
+        // Setupo runtime environment
+        let environment = Environment(isDebug: true,
+                                      messenger: messenger,
+                                      getScriptForModule: nil)
+        
+        // Then create & run intepreter
+        let interpreter = Interpreter(environment: environment)
+        
+        do {
+            try interpreter.runScript(script)
+            
+        } catch let error {
+            displayLog(message: "Error: \(error)")
+        }
+        
+        runButton.isEnabled = true
+    }
+    
+    private func displayLog(message: String) {
+        let text = logTextView.string + "\n" + message
+        logTextView.string = text
+    }
+    
+    private func saveScript(_ script: String) {
+        UserDefaults.standard.setValue(script, forKey: "edited-script")
+        UserDefaults.standard.synchronize()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func saveButtonTapped(sender: NSButton) {
+        saveScript(sourceTextView.string)
+    }
+    
+    @IBAction func runButtonTapped(sender: NSButton) {
+        runScript(sourceTextView.string + "\n")
+    }
+
+}
+
