ecdd4750b011fa8ed354f5dd35633bf8cdc6a95f
poisson florent <poissonflorent@MacBook-Pro-de-poisson-2.local>
2018-08-03 17:31:57 +0200

    Add Package class

--- Sources/Hop/Interpreter/Package.swift
+++ Sources/Hop/Interpreter/Package.swift
@@ -11,24 +11,32 @@ import Foundation
 
 /**
  
- A package is container for modules
-
- */
-/*
- 
- import Package1.module1 as mod1
-
- Physiquement un package correspond à un répertoire
- 
- On pourrait avoir : BreakoutGame package
- 
- - Dans l'app, on entre dans le packgae BreakoutGame => on identifier le script principal (fichier main.(...) et on l'exécute.
- 
- - En ligne de commande, on lance l'exe de l'interpreter en lui passant le répertoir d'exécution, ainsi que les répertoires de recherche des packages de dépendance, et on passe le fichier main.
+ A package is container for subpackages & modules
  
  */
 
-class Package_ {
+class Package: Evaluable {
+    var debugInfo: DebugInfo?
 
+    let name: String
+    let hashId: Int
+    let scope: Scope
+    
+    init(name: String, scope: Scope) {
+        self.name = name
+        self.hashId = name.hashValue
+        self.scope = scope
+        self.scope.name = name // Module scope is named
+    }
+    
+    var description: String {
+        return "Package(\(name))"
+    }
+    
+    func evaluate(context: Scope,
+                  session: Session) throws -> Evaluable? {
+        return self
+    }
     
 }
+
