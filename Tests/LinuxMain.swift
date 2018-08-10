18d08c118389578e7cb5e571139703e142cf7a06
poisson florent <poissonflorent@MacBook-Air-de-poisson.local>
2018-08-10 14:19:34 +0200

    Add Test module & Array test cases for linux tests run

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -5,4 +5,6 @@ import XCTest
 XCTMain([
     testCase(LexerTests.allTests),
     testCase(ParserErrorTests.allTests),
+    testCase(TestModuleTests.allTests),
+    testCase(ArrayClassTests.allTests)
     ])
